8206331092e3c7338c8687872e8d4289102f76ee
jrzax <jrzax@aol.com>
2020-10-28 12:54:59 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+This project contains the necessary firebase functions for Smarter Teams in Slack.
+
+## How to Use
+
+- All git commands shoul be run in the SmarterSlack directory
+- To deploy a new function you must first navigate to the functions folder
+    - From there, make sure node_modules is up to date by running npm install
+    - next, you can run firebase deploy FROM INSIDE THE FUNCTIONS FOLDER, to deploy to firebase
+
---
+++ functions/index.js
@@ -0,0 +1,49 @@
+const functions = require('firebase-functions');
+const axios = require('axios')
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.BadLanguage = functions.https.onRequest((request, response) => {
+    const pottywords = ["butthead", "crap", "stupid"]
+    if (request){
+        functions.logger.log("Here's the request:", request.body)
+        console.log("A request")
+        response.status(200).send(request.body)
+        if (request.body.event.subtype == "bot_message"){
+            return
+        } else {
+            let message = request.body.event.text
+            for (let i = 0; i < pottywords.length; i++){
+                if(message.match(pottywords[i])) {
+                    axios
+                    .post('', {
+                        text: `Rude word ${pottywords[i]} detected. Please consider using kinder language.`
+                    })
+                    .then(res => {
+                        functions.logger.log(`statusCode: ${res.statusCode}`)
+                        functions.logger.log(res)
+                    })
+                    .catch(error => {
+                        functions.logger.log(error)
+                    })
+                    return
+                }
+            }
+        }
+
+    } else {
+        throw response.status(500)
+    }
+});
+
+exports.AlphaDomination = functions.https.onRequest((request, response) => {
+    if (request){
+        functions.logger.log("Here's the request:", request.body)
+        console.log("A request")
+        response.status(200).send(request.body.challenge)
+
+    } else {
+        throw response.status(500)
+    }
+});
