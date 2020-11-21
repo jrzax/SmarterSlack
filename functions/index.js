e7e60d2563240be39c6e9f494bac59d28a2cd923
jrzax <jrzax@aol.com>
2020-11-20 20:20:42 -0600

    First Major implementation

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const admin = require("firebase-admin");
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "CHANGE ME",
+  apiKey: "CHANGE ME (COPY FROM FIRESTORE PROJECT SETTINGS)",
   authDomain: "smarter-teams-in-slack.firebaseapp.com",
   databaseURL: "https://smarter-teams-in-slack.firebaseio.com",
   projectId: "smarter-teams-in-slack",
@@ -27,9 +27,13 @@ const db = admin.firestore();
 
 exports.AlphaDomination = functions.https.onRequest(
   async (request, response) => {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
 
-    const usersRef = db.collection("users");
-    const channelsRef = db.collection("channels");
+    today = mm + '/' + dd + '/' + yyyy;
+
     const pottywords = ["butthead", "crap", "stupid"];
 
     if (request) {
@@ -49,29 +53,110 @@ exports.AlphaDomination = functions.https.onRequest(
             badWordsCount += 1;
           } 
         }
+
+        functions.logger.log(today)
+
+        //This function is now a beast. First we set userRef to be the document in the users collection
+        // with this current userID
         const userRef = db.collection("users").doc(userID);
-        userRef.get().then((docSnapshot) => {
-          if (docSnapshot.exists) {
+
+        //We then check to see if the user's history collection has a document with todays date
+        // oof, I know. Check the firestore to see the structure
+        userRef.collection('history').where('date', '==', today).get().then((querySnapshot) => {
+
+          let found = false
+          //Looping through matching documents. there should only be one
+          querySnapshot.forEach((doc) => {
+            found = true
+            //updating daily message total
+            userRef.collection('history').doc(doc.id).update({
+              numMessages: admin.firestore.FieldValue.increment(1),
+              numBadWords: admin.firestore.FieldValue.increment(badWordsCount)
+            })
+
+            //updating total user message total
             userRef.update({
               numMessages: admin.firestore.FieldValue.increment(1),
               numBadWords: admin.firestore.FieldValue.increment(badWordsCount)
             })
-          } else {
-            userRef
-            .set({ numMessages: 0, numBadWords: badWordsCount });
+          });
+
+          //If there are no documents with the matching day, the user hasn't messaged on that day
+          // OR the user doesn't exist
+          if (!found) {
+            //We need to get the user document to see if it exists
+            userRef.get().then((snapshot) => {
+
+              //Testing to see if the user exists
+              if (snapshot.exists) {
+                //User exists, update add a document for a new day, update total counts
+                userRef.collection('history').add({
+                  numMessages: 1, numBadWords: badWordsCount, date: today 
+               })
+               userRef.update({
+                 numMessages: admin.firestore.FieldValue.increment(1),
+                 numBadWords: admin.firestore.FieldValue.increment(badWordsCount)
+               })
+              } else {
+                functions.logger.log("no user")
+                //user is not found, we need to make a new one, initializing counts
+                userRef.set({
+                  numMessages: 1,
+                  numBadWords: badWordsCount
+                })
+                //now adding a document for new day in user's history collection
+                userRef
+                  .collection('history')
+                  .add({ numMessages: 1, numBadWords: badWordsCount, date: today });
+              }
+            })
           }
+        }).catch((error) => {
+          functions.logger.log(error)
         })
 
+        //This Literally repeats the same code done above, the only difference is this time
+        // we use channelRef, to change where we are looking in firebase
         const channelRef = db.collection("channels").doc(channelName);
-        await channelRef.get().then((docSnapshot) => {
-          if (docSnapshot.exists) {
-            channelRef.update({
-              numMessages: admin.firestore.FieldValue.increment(1)
+        channelRef.collection('history').where('date', '==', today).get().then((querySnapshot) => {
+          let found = false
+          querySnapshot.forEach((doc) => {
+            found = true
+            channelRef.collection('history').doc(doc.id).update({
+              numMessages: admin.firestore.FieldValue.increment(1),
+              numBadWords: admin.firestore.FieldValue.increment(badWordsCount)
             })
-          } else {
-            channelRef
-            .set({ numMessages: 1 });
+            channelRef.update({
+              numMessages: admin.firestore.FieldValue.increment(1),
+              numBadWords: admin.firestore.FieldValue.increment(badWordsCount)
+            })
+          });
+          if (!found) {
+            channelRef.get().then((snapshot) => {
+              //Testing to see if the channel exists
+              if (snapshot.exists) {
+                channelRef.collection('history').add({
+                  numMessages: 1, numBadWords: badWordsCount, date: today 
+               })
+               channelRef.update({
+                 numMessages: admin.firestore.FieldValue.increment(1),
+                 numBadWords: admin.firestore.FieldValue.increment(badWordsCount)
+               })
+              } else {
+                //channel is not found, we need to make a new one
+                functions.logger.log("no channel")
+                channelRef.set({
+                  numMessages: 1,
+                  numBadWords: badWordsCount
+                })
+                channelRef
+                  .collection('history')
+                  .add({ numMessages: 1, numBadWords: badWordsCount, date: today });
+              }
+            })
           }
+        }).catch((error) => {
+          functions.logger.log(error)
         })
 
         response.status(200).send(request.body);
