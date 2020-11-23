5309f5a7e1878207a2994d7549b5602b0b7334fa
jrzax <jrzax@aol.com>
2020-11-22 22:23:53 -0600

    Finishing up reporting

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const admin = require("firebase-admin");
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "CHANGE ME (COPY FROM FIRESTORE PROJECT SETTINGS)",
+  apiKey: "Change Me (Get From Firebase project settings)",
   authDomain: "smarter-teams-in-slack.firebaseapp.com",
   databaseURL: "https://smarter-teams-in-slack.firebaseio.com",
   projectId: "smarter-teams-in-slack",
@@ -28,6 +28,7 @@ const db = admin.firestore();
 exports.AlphaDomination = functions.https.onRequest(
   async (request, response) => {
     var today = new Date();
+
     var dd = String(today.getDate()).padStart(2, '0');
     var mm = String(today.getMonth() + 1).padStart(2, '0');
     var yyyy = today.getFullYear();
@@ -38,10 +39,12 @@ exports.AlphaDomination = functions.https.onRequest(
 
     if (request) {
       functions.logger.log("NEW RUN STARTS HERE");
-      // functions.logger.log("Here's the request:", request.body);
+      functions.logger.log("Here's the request:", request.body);
       if (request.body.event.subtype == "bot_message") {
+        response.status(200).send(request.body)
         return;
       } else {
+        functions.logger.log(request)
         let userID = request.body.event.user; //get user id from sent message
         let channelName = request.body.event.channel; //new doc being created for new channel
         
@@ -63,7 +66,6 @@ exports.AlphaDomination = functions.https.onRequest(
         //We then check to see if the user's history collection has a document with todays date
         // oof, I know. Check the firestore to see the structure
         userRef.collection('history').where('date', '==', today).get().then((querySnapshot) => {
-
           let found = false
           //Looping through matching documents. there should only be one
           querySnapshot.forEach((doc) => {
@@ -166,6 +168,66 @@ exports.AlphaDomination = functions.https.onRequest(
     }
   }
 );
+
+exports.SmartReport = functions.https.onRequest(
+  async (request, response) => {   
+    if (request){    
+      functions.logger.log("Here's the request:", request.body)      
+      console.log("A request")      
+
+      const usersRef = db.collection('users')
+
+      let result = {}
+      await usersRef.get().then(querySnapshot => {
+
+        // let result = {userID: [10, 9]}
+        // result[userId][0]
+        // to get userId result[0]
+        querySnapshot.forEach((doc) => {
+          let data = doc.data()
+          result[doc.id] = [data.numMessages, data.numBadWords]
+        });
+
+      }).catch((error) => {
+        functions.logger.log(error)
+        result = false
+      })
+
+      functions.logger.log(result)
+      // result.sort((a,b) => {
+      //   return a[0] - b[0]
+      // })
+
+      // let alphaList = result
+
+      // result.sort((a,b) => {
+      //   return a[1] - b[1]
+      // })
+
+      // let badList = result
+
+      // let text = ''
+      // let count = 1
+      // alphaList.forEach((element) => {
+      //   text.push(count + '. ' + element[0])
+      // })
+
+
+      let text = JSON.stringify(result)
+      //change post endpoint before pushing to github
+      axios
+            .post("Change Me (from Slack API)", {
+              text: text,
+            })
+            .then((res) => {
+            })
+            .catch((error) => {
+              functions.logger.log(error);
+            }); 
+    response.status(200).send(request.body.challenge)
+    } else {        
+      throw response.status(500)    
+    }});
 
  // console.log("Document data:", doc.data());
           // axios
